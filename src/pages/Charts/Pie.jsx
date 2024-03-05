e1e4367c53c17c0415fa39c4afcabb74702e381e
unknown <zulfikrimirza@yahoo.com>
2024-03-05 14:21:26 +0700

    bar chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
+
+import { pieChartData } from '../../data/dummy'
+import { Header } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider'
+
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Pie" title="Project Cost Breakdown" />
+      <div className='w-full'>
+        Pie
+      </div>
+    </div>
   )
 }
 
